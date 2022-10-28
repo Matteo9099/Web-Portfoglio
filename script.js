d68b6213461fc895d942e9fc444418ebeec14b6c
Matteo Morganti <matteo99morganti@gmail.com>
2022-10-28 20:12:16 +0200

    download cv

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 // Header Scroll
 let header = document.querySelector('.header');
+let title_profilo = document.getElementById('title_profilo');
+let title_name = document.getElementById('title_name');
 window.addEventListener('scroll',function(){
     if(window.scrollY > 200) {
         header.classList.add('nav_scroll');
+        title_profilo.classList.add('opacity');
+        title_name.classList.remove('opacity');
     } else {
         header.classList.remove('nav_scroll');
+        title_profilo.classList.remove('opacity');
+        title_name.classList.add('opacity');
     }
 })
 
+
 // SetTimeout name 
 let name = document.querySelector('.name_matteo_morganti');
+let prof = document.querySelector('.profession_text');
 setTimeout(function(){
     name.classList.add('opacity_show');
     name.classList.remove('opacity')
 }, 800);
+setTimeout(function(){
+    prof.classList.add('opacity_show');
+    prof.classList.remove('opacity')
+}, 1100)
 
 
 // SetTimeout image 
@@ -41,6 +53,6 @@ ScrollReveal().reveal('.reveal', {
     distance: '50px', 
     duration: 1500,
     easing: 'cubic-bezier(.215, .61, .355, 1)',
-    interval: 700,
+    interval: 500,
     delay: 300
 });
